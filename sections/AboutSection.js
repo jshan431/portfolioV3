75988d080f06f35af01d01383ed45efaa138b4c5
jshan431 <58716914+jshan431@users.noreply.github.com>
2022-09-13 22:59:44 -0700

    have mobile view about section all set to active

--- sections/AboutSection.js
+++ sections/AboutSection.js
@@ -5,6 +5,7 @@ import AboutMe from '../components/AboutMe';
 import Photos from '../components/Photos';
 import Contact from '../components/Contact';
 import Clients from '../components/Clients';
+import { useMediaQuery } from 'react-responsive'
 export default function AboutSection() {
 
   const [selectedPanel, setSelectedPanel] = useState('first')
@@ -13,24 +14,57 @@ export default function AboutSection() {
     setSelectedPanel(str)
   }
 
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 931px)'
+  })
+
+
   return (
     <section className={styles.container}>
-      <div className={`${styles.panel} ${styles.panel01} ${selectedPanel === 'first' ? styles.active : ''}`} onClick={() => panelClick('first')}>
-        <AboutMe selectedPanel={selectedPanel}/>
-        <h3>About Me</h3>
-      </div>
-      <div className={`${styles.panel} ${styles.panel02} ${selectedPanel === 'second' ? styles.active : ''}`} onClick={() => panelClick('second')}>
-        <Technologies selectedPanel={selectedPanel}/>
-        <h3>Technologies</h3>
-      </div>
-      <div className={`${styles.panel} ${styles.panel03} ${selectedPanel === 'third' ? styles.active : ''}`} onClick={() => panelClick('third')}>
-        <Photos selectedPanel={selectedPanel}/>
-        <h3>Photos</h3>
-      </div>
-      <div className={`${styles.panel} ${styles.panel04} ${selectedPanel === 'fourth' ? styles.active : ''}`} onClick={() => panelClick('fourth')}>
-        <Clients/>
-        <h3>Clients</h3>
-      </div>
+      {isDesktopOrLaptop ?
+        <div className={`${styles.panel} ${styles.panel01} ${selectedPanel === 'first' ? styles.active : ''}`} onClick={() => panelClick('first')}>
+          <AboutMe selectedPanel={selectedPanel}/>
+          <h3>About Me</h3>
+        </div>
+        :
+        <div className={`${styles.panel} ${styles.panel01} ${styles.active}`}>
+          <AboutMe selectedPanel={'first'}/>
+          <h3>About Me</h3>
+        </div>
+      }
+      {isDesktopOrLaptop ?
+        <div className={`${styles.panel} ${styles.panel02} ${selectedPanel === 'second' ? styles.active : ''}`} onClick={() => panelClick('second')}>
+          <Technologies selectedPanel={selectedPanel}/>
+          <h3>Technologies</h3>
+        </div>
+        :
+        <div className={`${styles.panel} ${styles.panel02} ${styles.active }`}>
+          <Technologies selectedPanel={'second'}/>
+          <h3>Technologies</h3>
+        </div>
+      }
+      {isDesktopOrLaptop ?
+        <div className={`${styles.panel} ${styles.panel03} ${selectedPanel === 'third' ? styles.active : ''}`} onClick={() => panelClick('third')}>
+          <Photos selectedPanel={selectedPanel}/>
+          <h3>Photos</h3>
+        </div>
+      :
+        <div className={`${styles.panel} ${styles.panel03} ${styles.active}`}>
+          <Photos selectedPanel={'third'}/>
+          <h3>Photos</h3>
+        </div>
+      }
+      {isDesktopOrLaptop ?
+        <div className={`${styles.panel} ${styles.panel04} ${selectedPanel === 'fourth' ? styles.active : ''}`} onClick={() => panelClick('fourth')}>
+          <Clients/>
+          <h3>Clients</h3>
+        </div>
+      :
+        <div className={`${styles.panel} ${styles.panel04} ${styles.active} `}>
+          <Clients selectedPanel={'fourth'}/>
+          <h3>Clients</h3>
+        </div>
+      }
       <div className={`${styles.panel} ${styles.panel05} ${selectedPanel === 'fifth' ? styles.active : ''}`} onClick={() => panelClick('fifth')}>
         <Contact selectedPanel={selectedPanel}/>
         <h3>Contact</h3>
@@ -40,3 +74,13 @@ export default function AboutSection() {
 }
 
 // style={{backgroundImage: "url('https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80')"}}
+
+/**
+
+
+        <div className={`${styles.panel} ${styles.panel01} ${styles.active}`}>
+          <AboutMe selectedPanel={selectedPanel}/>
+          <h3>About Me</h3>
+        </div>
+
+ */
