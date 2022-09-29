f84a923a5e43c018668110f631b3cdc48d1f7bf6
jshan431 <58716914+jshan431@users.noreply.github.com>
2022-09-29 16:26:55 -0700

    add rerender on resize on home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useMediaQuery } from 'react-responsive'
+import { useEffect, useState } from 'react'
 import styles from '../styles/pages/Home.module.css'
 import HeroSection from '../sections/HeroSection'
 import AboutMe from '../sections/AboutMe'
@@ -13,12 +14,34 @@ export default function Home() {
     query: '(min-width: 1224px)'
   })
 
+  const [dimensions, setDimensions] = useState({ 
+    height: undefined,
+    width: undefined
+  })
+
+  useEffect(() => {
+
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return _ => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })  
+
   return (
 
     <div className={styles.main}>
       <HeroSection />
       <AboutMe />
       {isDesktopOrLaptop ? <FeaturedProjects /> : <FeaturedProjectsMobile />}
+
     </div>
   )
 }
