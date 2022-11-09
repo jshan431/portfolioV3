032eb13bd8de644840fd2ac6bcce751ffe6c7082
jshan431 <58716914+jshan431@users.noreply.github.com>
2022-11-08 23:55:17 -0800

    fix quote generator naming

--- public/data/projects.js
+++ public/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
   {
     "id": "1",
     "name": "Quote Generator",
-    "slug": "QuoteGenerator",
+    "slug": "quoteGenerator",
     "description": "A quote generator that fetches from an API that provides over 1600 quotes and randomly displays one. It also allows the user to Tweet out the generated quote.",
     "image": "/images/projects/quoteGenerator.png"
   }
