38259834f9da6030d5408df569335f4618626389
jshan431 <58716914+jshan431@users.noreply.github.com>
2022-11-10 15:56:03 -0800

    add first version of joke teller

--- public/data/projects.js
+++ public/data/projects.js
@@ -6,7 +6,14 @@ const projects = [
     "slug": "quoteGenerator",
     "description": "A quote generator that fetches from an API that provides over 1600 quotes and randomly displays one. It also allows the user to Tweet out the generated quote.",
     "image": "/images/projects/quoteGenerator.png"
-  }
+  },
+  {
+    "id": "2",
+    "name": "Joke Teller",
+    "slug": "jokeTeller",
+    "description": "A quote generator that fetches from an API that provides over 1600 quotes and randomly displays one. It also allows the user to Tweet out the generated quote.",
+    "image": "/images/projects/quoteGenerator.png"
+  },
 ]
 
 export default projects;
